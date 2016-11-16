ad217c5b2ebec0605e5b3423882d47fe9b9b31b4
excilys <excilys@desktop-0320.ebi.excilys.com>
2016-11-16 17:58:30 +0100

    add tag

--- src/main/java/com/excilys/formation/computerdatabase/web/tag/PageTag.java
+++ src/main/java/com/excilys/formation/computerdatabase/web/tag/PageTag.java
@@ -1,5 +1,45 @@
-package com.excilys.formation.computerdatabase.web.tag;
+package com.journaldev.jsp.customtags;
 
-public class PageTag {
+import java.io.IOException;
+import java.text.DecimalFormat;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.SkipPageException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+import com.excilys.formation.computerdatabase.model.Company;
+import com.excilys.formation.computerdatabase.model.Page;
+
+public class PageTag extends SimpleTagSupport {
+
+  private Page<Company> pages;
+
+  public PageTag() {
+  }
+
+  public void setFormat(String format) {
+    this.format = format;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  @Override
+  public void doTag() throws JspException, IOException {
+    System.out.println("Number is:" + number);
+    System.out.println("Format is:" + format);
+    try {
+      double amount = Double.parseDouble(number);
+      DecimalFormat formatter = new DecimalFormat(format);
+      String formattedNumber = formatter.format(amount);
+      getJspContext().getOut().write(formattedNumber);
+    } catch (Exception e) {
+      e.printStackTrace();
+      // stop page from loading further by throwing SkipPageException
+      throw new SkipPageException("Exception in formatting " + number
+          + " with format " + format);
+    }
+  }
 
 }
